34c2951b9c5754c997af73cbc49cdc1a62d4dcf1
Brayan Diaz C <brayandiazc@gmail.com>
2025-07-17 17:37:52 -0500

    Fix welcome message in index route for clarity

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Bienvenido al proyecto DevOps educativo con un cambio"
+    return "Bienvenido al proyecto DevOps educativo con cambios"
 
 @app.route('/health')
 def health():
